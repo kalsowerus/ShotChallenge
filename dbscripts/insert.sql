48fff1d730bcb4f7e177cfe7d48dc016d7f3c43b
kalsowerus <kalsowerus@protonmail.ch>
2022-01-29 01:53:08 +0100

    Add insert db script

---
+++ dbscripts/insert.sql
@@ -0,0 +1,11 @@
+INSERT INTO Team (name)
+VALUES
+       ('Oktaväsumpfer Büsserach'),
+       ('Hälmlibieger Röschenz'),
+       ('Hirzefäger Breitenbach');
+
+INSERT INTO Shot (name)
+VALUES
+       ('Bloody Mary'),
+       ('Blue Kamikaze'),
+       ('Berliner Luft');
